785a28aaa25c5aded7fee4e6757d45e28c4034bc
Heiko Marschner <heiko.marschner@gmail.com>
2021-01-01 21:01:28 +0100

    Send events in case of changes only

--- jobs/icinga2.rb
+++ jobs/icinga2.rb
@@ -23,20 +23,13 @@ require './lib/icinga2'
 icinga = Icinga2.new('config/icinga2.json') # fixed path
 
 SCHEDULER.every '15s', :first_in => 0 do |job|
+  # shallow copy of the icinga object to detect changes in values
+  icinga_previous = icinga.dup
   # run data provider
   icinga.run
 
   #puts "App Info: " + icinga.app_data.to_s + " Version: " + icinga.version
   #puts "CIB Info: " + icinga.cib_data.to_s
-
-  # meter widget
-  # we'll update the patched meter widget with absolute values (set max dynamically)
-  host_meter = icinga.host_count_problems.to_f
-  host_meter_max = icinga.host_count_all
-  service_meter = icinga.service_count_problems.to_f
-  service_meter_max = icinga.service_count_all
-
-  #puts "Meter widget: Hosts " + host_meter.to_s + "/" + host_meter_max.to_s + " Services " + service_meter.to_s + "/" + service_meter_max.to_s
 
   # icinga stats
   icinga_stats = [
@@ -62,17 +55,23 @@ SCHEDULER.every '15s', :first_in => 0 do |job|
   #puts "Stats: " + icinga_stats.to_s
 
   ### Events
-  send_event('icinga-host-meter', {
-   value: host_meter,
-   max:   host_meter_max,
-   moreinfo: "Total hosts: " + host_meter_max.to_s,
-   color: 'blue' })
+  if icinga.host_count_problems != icinga_previous.host_count_problems or
+     icinga.host_count_all != icinga_previous.host_count_all
+    send_event('icinga-host-meter', {
+      value: icinga.host_count_problems,
+      max:   icinga.host_count_all,
+      moreinfo: "Total hosts: " + icinga.host_count_all.to_s,
+      color: 'blue' })
+  end
 
-  send_event('icinga-service-meter', {
-   value: service_meter,
-   max:   service_meter_max,
-   moreinfo: "Total services: " + service_meter_max.to_s,
-   color: 'blue' })
+  if icinga.service_count_problems != icinga_previous.service_count_problems or
+     icinga.service_count_all != icinga_previous.service_count_all
+    send_event('icinga-service-meter', {
+      value: icinga.service_count_problems,
+      max:   icinga.service_count_all,
+      moreinfo: "Total services: " + icinga.service_count_all.to_s,
+      color: 'blue' })
+  end
 
   send_event('icinga-stats', {
    title: "Icinga " + icinga.version,
@@ -80,76 +79,82 @@ SCHEDULER.every '15s', :first_in => 0 do |job|
    color: 'blue' })
 
   #### Doughnuts
-  send_event('doughnut-pie-hosts', {
-    type: "doughnut",
-    header: "Hosts",
-    labels: [ "UP", "Down" ],
-    datasets: [ icinga.host_count_up, icinga.host_count_problems_down],
-    moreinfo: "Total hosts: " + icinga.host_count_all.to_s,
-  })
+  if icinga.host_count_up != icinga_previous.host_count_up or
+     icinga.host_count_problems_down != icinga_previous.host_count_problems_down
 
-  send_event('doughnut-pie-services', {
-    type: "doughnut",
-    header: "Services",
-    labels: [ "OK", "Warning", "Critical", "Unknown" ],
-    datasets: [ icinga.service_count_ok, icinga.service_count_problems_warning, icinga.service_count_problems_critical, icinga.service_count_problems_unknown],
-    moreinfo: "Total services: " + icinga.service_count_all.to_s,
-  })
+    moreinfo_msg = "Total hosts: " + icinga.host_count_all.to_s
+    if icinga.host_count_problems_down > 0
+      moreinfo_msg += " (Down: " + icinga.host_count_problems_down.to_s + ")"
+    end
+
+    send_event('doughnut-pie-hosts', {
+      type: "doughnut",
+      header: "Hosts",
+      labels: [ "UP", "Down" ],
+      datasets: [ icinga.host_count_up, icinga.host_count_problems_down ],
+      moreinfo: moreinfo_msg,
+    })
+  end
+
+  if icinga.service_count_ok != icinga_previous.service_count_ok or
+     icinga.service_count_problems_warning != icinga_previous.service_count_problems_warning or
+     icinga.service_count_problems_critical != icinga_previous.service_count_problems_critical or
+     icinga.service_count_problems_unknown != icinga_previous.service_count_problems_unknown
+
+    moreinfo_msg = "Total services: " + icinga.service_count_all.to_s
+    service_count_not_ok = icinga.service_count_problems_warning + icinga.service_count_problems_critical + icinga.service_count_problems_unknown
+    if service_count_not_ok > 0
+      moreinfo_msg += " (Not OK: " + service_count_not_ok.to_s + ")"
+    end
+
+    send_event('doughnut-pie-services', {
+      type: "doughnut",
+      header: "Services",
+      labels: [ "OK", "Warning", "Critical", "Unknown" ],
+      datasets: [ icinga.service_count_ok, icinga.service_count_problems_warning, icinga.service_count_problems_critical, icinga.service_count_problems_unknown ],
+      moreinfo: moreinfo_msg,
+    })
+  end
 
   send_event('bar-chart-endpoints', {
     header: "Endpoints",
     labels: [ "Connected", "Not Connected" ],
-    datasets: [ clusterStats["num_conn_endpoints"], clusterStats["num_not_conn_endpoints"]],
+    datasets: [ clusterStats["num_conn_endpoints"], clusterStats["num_not_conn_endpoints"] ],
   })
 
   #### Bar Charts
-  send_event('bar-chart-checks', {
-    #type: "horizontalBar",
-    type: "bar",
-    header: "Active Checks",
-    labels: [ "Hosts/min", "Services/min" ],
-    datasets: [ icinga.host_active_checks_1min, icinga.service_active_checks_1min],
-  })
-  send_event('bar-chart-downtimes', {
-    #type: "horizontalBar",
-    type: "bar",
-    header: "Downtimes",
-    labels: [ "Hosts", "Services" ],
-    datasets: [ icinga.host_count_in_downtime, icinga.service_count_in_downtime],
-  })
-  send_event('bar-chart-acks', {
-    #type: "horizontalBar",
-    type: "bar",
-    header: "Acknowledgements",
-    labels: [ "Hosts", "Services" ],
-    datasets: [ icinga.host_count_acknowledged, icinga.service_count_acknowledged],
-  })
+  if icinga.host_active_checks_1min != icinga_previous.host_active_checks_1min or
+     icinga.service_active_checks_1min != icinga_previous.service_active_checks_1min
+    send_event('bar-chart-checks', {
+      #type: "horizontalBar",
+      type: "bar",
+      header: "Active Checks",
+      labels: [ "Hosts/min", "Services/min" ],
+      datasets: [ icinga.host_active_checks_1min, icinga.service_active_checks_1min ],
+    })
+  end
 
-#  send_event('icinga-host-meter', {
-#   value: host_meter,
-#   max:   host_meter_max,
-#   moreinfo: "Total hosts: " + host_meter_max.to_s,
-#   color: 'blue' })
-#
-#  send_event('icinga-service-meter', {
-#   value: service_meter,
-#   max:   service_meter_max,
-#   moreinfo: "Total services: " + service_meter_max.to_s,
-#   color: 'blue' })
+  if icinga.host_count_in_downtime != icinga_previous.host_count_in_downtime or
+     icinga.service_count_in_downtime != icinga_previous.service_count_in_downtime
+    send_event('bar-chart-downtimes', {
+      #type: "horizontalBar",
+      type: "bar",
+      header: "Downtimes",
+      labels: [ "Hosts", "Services" ],
+      datasets: [ icinga.host_count_in_downtime, icinga.service_count_in_downtime ],
+    })
+  end
 
-  # handled stats
-#  handled_stats = [
-#    {"label" => "Acknowledgements", "color" => "blue"},
-#    {"label" => "Hosts", "value" => icinga.host_count_acknowledged},
-#    {"label" => "Services", "value" => icinga.service_count_acknowledged},
-#    {"label" => "Downtimes", "color" => "blue"},
-#    {"label" => "Hosts", "value" => icinga.host_count_in_downtime},
-#    {"label" => "Services", "value" => icinga.service_count_in_downtime},
-#  ]
-#
-#  send_event('handled-stats', {
-#   items: handled_stats,
-#   color: 'blue' })
+  if icinga.host_count_acknowledged != icinga_previous.host_count_acknowledged or
+     icinga.service_count_acknowledged != icinga_previous.service_count_acknowledged
+    send_event('bar-chart-acks', {
+      #type: "horizontalBar",
+      type: "bar",
+      header: "Acknowledgements",
+      labels: [ "Hosts", "Services" ],
+      datasets: [ icinga.host_count_acknowledged, icinga.service_count_acknowledged ],
+    })
+  end
 
   # problem services
   severity_stats = []
@@ -210,16 +215,22 @@ SCHEDULER.every '15s', :first_in => 0 do |job|
     moreinfo: "All Problems: " + (icinga.service_count_problems_critical + icinga.service_count_problems_warning + icinga.service_count_problems_unknown).to_s
   })
 
-  send_event('icinga-room-climate', {
-    current: icinga.room_climate_temperature,
-    suffix: "°C",
-    moreinfo: "Humidity: " + icinga.room_climate_humidity.round.to_s + " %H"
-  })
+  if icinga.room_climate_temperature != icinga_previous.room_climate_temperature or
+     icinga.room_climate_humidity != icinga_previous.room_climate_humidity
+    send_event('icinga-room-climate', {
+      current: icinga.room_climate_temperature,
+      suffix: "°C",
+      moreinfo: "Humidity: " + icinga.room_climate_humidity.round.to_s + " %H"
+    })
+  end
 
-  send_event('icinga-isp', {
-    downstream: icinga.isp_downstream.round,
-    upstream: icinga.isp_upstream.round,
-    unitinfo: "In Mbit/s"
-  })
+  if icinga.isp_downstream != icinga_previous.isp_downstream or
+     icinga.isp_upstream != icinga_previous.isp_upstream
+    send_event('icinga-isp', {
+      downstream: icinga.isp_downstream.round,
+      upstream: icinga.isp_upstream.round,
+      unitinfo: "In Mbit/s"
+    })
+  end
 end
 
